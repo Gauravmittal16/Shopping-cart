8909d2497aa59c0ba1dcd448b1af750ec401b837
gaurav <gauravmittal1609@gmail.com>
2021-07-06 21:40:06 +0530

    comments added for apis

--- src/main/java/com/stu/demo/controller/SiteController.java
+++ src/main/java/com/stu/demo/controller/SiteController.java
@@ -24,18 +24,22 @@ public class SiteController
 	{
 		return "homepage.jsp";
 	}
-	
+	/*API to add Products*/
 	@RequestMapping(value="/addProduct",method= {RequestMethod.POST,RequestMethod.GET})
 	public String addCandidate(@RequestBody Candidate candidate)
 	{
 		return candidateService.saveCandidate(candidate);
 	}
+	
+	/*API to fetch Products*/
 	@RequestMapping("/products")
 	@ResponseBody
 	public List<Candidate> getCandidates()
 	{
 		return candidateService.getCandites();
 	}
+	
+	/*API to fetch Product*/
 	@RequestMapping("/products/{id}")
 	@ResponseBody
 	public Candidate getCandidate(@PathVariable("id") int id)
@@ -43,12 +47,14 @@ public class SiteController
 		return candidateService.getCandet(id);
 	}
 	
+	/*API to update Product*/
 	@RequestMapping(value="/update",method= {RequestMethod.PUT,RequestMethod.GET})
 	public String updateCandidate(@RequestBody Candidate candidate)
 	{
 		return candidateService.UpdateCandidate(candidate);
 	}
 	
+	/*API to delete Product*/
 	@RequestMapping(value="/delete/{id}", method={RequestMethod.DELETE,RequestMethod.GET})
 	@ResponseBody
 	public ResponseEntity<String> deleteCandidate(@PathVariable("id") int id)
